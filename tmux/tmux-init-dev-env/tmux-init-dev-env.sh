ead9cde22e02263f3338644d59e9859a23f86883
vagrant <vagrant@mc-dev>
2016-08-25 06:01:57 +0000

    add template

--- tmux/tmux-init-dev-env/tmux-init-dev-env.sh
+++ tmux/tmux-init-dev-env/tmux-init-dev-env.sh
@@ -15,56 +15,63 @@ vim='vim -u ~/.spf13-vim-3/.vimrc'
 
 # mysql
 mysql='mysql'
+mysql_host='localhost'
 mysql_user='user'
 mysql_password='password'
 
 # redis
 redis_cli='/opt/redis/bin/redis-cli'
+redis_cli_host='localhost'
 redis_cli_port='6000'
 redis_cli_password='redis123456'
 
 # 项目名称
-project_call='mc_work_call'
-project_michael='mc_work_michael'
+project_template='work-template'
+project_michael='work-michael'
 
 # 初始化项目环境
-function init_project_call()
+function init_project_template()
 {
+    # work path
+    local work_path='/home/vagrant/code/template'
+    cd ${work_path}
+
     local cmd_debug_log='touch /tmp/debug_`date +%Y%m%d`.log && chown nobody:nobody /tmp/debug_`date +%Y%m%d`.log && tail -f /tmp/debug_`date +%Y%m%d`.log'
 
-    cd /home/html/call
-
-    ${tmux} -2 new-session -d -s ${project_call}
+    ${tmux} -2 new-session -d -s ${project_template}
 
     # vim
-    ${tmux} new-window -d -n vim -t ${project_call}
-    ${tmux} send-keys -t "${project_call}:vim" 'cd /home/html/call' C-m
-    ${tmux} send-keys -t "${project_call}:vim" "${vim}" C-m
+    ${tmux} new-window -d -n vim -t ${project_template}
+    ${tmux} send-keys -t "${project_template}:vim" "cd ${work_path}" C-m
+    ${tmux} send-keys -t "${project_template}:vim" "${vim}" C-m
 
     # log
-    ${tmux} new-window -d -n log -t ${project_call}
-    ${tmux} send-keys -t "${project_call}:log" "${cmd_debug_log}" C-m
+    ${tmux} new-window -d -n log -t ${project_template}
+    ${tmux} send-keys -t "${project_template}:log" "${cmd_debug_log}" C-m
 
-    # svn
-    ${tmux} new-window -d -n svn -t ${project_call}
-    ${tmux} send-keys -t "${project_call}:svn" 'cd /home/html/call' C-m
+    # git
+    ${tmux} new-window -d -n git -t ${project_template}
+    ${tmux} send-keys -t "${project_template}:git" "cd ${work_path}" C-m
+
+#    # svn
+#    ${tmux} new-window -d -n svn -t ${project_template}
+#    ${tmux} send-keys -t "${project_template}:svn" "cd ${work_path}" C-m
 
     # mysql
-    ${tmux} new-window -d -n mysql -t ${project_call}
-    ${tmux} send-keys -t "${project_call}:mysql" "${mysql} -u${mysql_user} -p${mysql_password} call" C-m
+    ${tmux} new-window -d -n mysql -t ${project_template}
+    ${tmux} send-keys -t "${project_template}:mysql" "${mysql} -h${mysql_host} -u${mysql_user} -p${mysql_password} call" C-m
 
     # redis
-    ${tmux} new-window -d -n redis -t ${project_call}
-    ${tmux} send-keys -t "${project_call}:redis" "${redis_cli} -p ${redis_cli_port} -a ${redis_cli_password}" C-m
+    ${tmux} new-window -d -n redis -t ${project_template}
+    ${tmux} send-keys -t "${project_template}:redis" "${redis_cli} -h ${redis_cli_host} -p ${redis_cli_port} -a ${redis_cli_password}" C-m
 
     # workspace
-    ${tmux} new-window -d -n workspace -t ${project_call}
-    ${tmux} send-keys -t "${project_call}:workspace" 'cd /home/html/call' C-m
+    ${tmux} new-window -d -n workspace -t ${project_template}
+    ${tmux} send-keys -t "${project_template}:workspace" "cd ${work_path}" C-m
 
     # x
-    ${tmux} new-window -d -n x -t ${project_call}
-    ${tmux} send-keys -t "${project_call}:x" 'cd /home/html/call' C-m
-
+    ${tmux} new-window -d -n x -t ${project_template}
+    ${tmux} send-keys -t "${project_template}:x" "cd ${work_path}" C-m
 
     ${tmux} select-window -t vim
 
@@ -73,61 +80,67 @@ function init_project_call()
 # 初始化项目环境
 function init_project_michael()
 {
-    local cmd_debug_log='touch /tmp/debug_`date +%Y%m%d`.log && chown nobody:nobody /tmp/debug_`date +%Y%m%d`.log && tail -f /tmp/debug_`date +%Y%m%d`.log'
+    # work path
+    local work_path='/home/vagrant/code/michael'
+    cd ${work_path}
 
-    cd /home/html/michael
+    local cmd_debug_log='touch /tmp/debug_`date +%Y%m%d`.log && chown nobody:nobody /tmp/debug_`date +%Y%m%d`.log && tail -f /tmp/debug_`date +%Y%m%d`.log'
 
     ${tmux} -2 new-session -d -s ${project_michael}
 
     # vim
     ${tmux} new-window -d -n vim -t ${project_michael}
-    ${tmux} send-keys -t "${project_michael}:vim" 'cd /home/html/michael' C-m
+    ${tmux} send-keys -t "${project_michael}:vim" "cd ${work_path}" C-m
     ${tmux} send-keys -t "${project_michael}:vim" "${vim}" C-m
 
     # log
     ${tmux} new-window -d -n log -t ${project_michael}
     ${tmux} send-keys -t "${project_michael}:log" "${cmd_debug_log}" C-m
 
-    # svn
-    ${tmux} new-window -d -n svn -t ${project_michael}
-    ${tmux} send-keys -t "${project_michael}:svn" 'cd /home/html/michael' C-m
+    # git
+    ${tmux} new-window -d -n git -t ${project_michael}
+    ${tmux} send-keys -t "${project_michael}:git" "cd ${work_path}" C-m
+
+#    # svn
+#    ${tmux} new-window -d -n svn -t ${project_michael}
+#    ${tmux} send-keys -t "${project_michael}:svn" "cd ${work_path}" C-m
 
     # mysql
     ${tmux} new-window -d -n mysql -t ${project_michael}
-    ${tmux} send-keys -t "${project_michael}:mysql" "${mysql} -u${mysql_user} -p${mysql_password} " C-m
+    ${tmux} send-keys -t "${project_michael}:mysql" "${mysql} -h${mysql_host} -u${mysql_user} -p${mysql_password} call" C-m
 
     # redis
     ${tmux} new-window -d -n redis -t ${project_michael}
-    ${tmux} send-keys -t "${project_michael}:redis" "${redis_cli} -p ${redis_cli_port} -a ${redis_cli_password}" C-m
+    ${tmux} send-keys -t "${project_michael}:redis" "${redis_cli} -h ${redis_cli_host} -p ${redis_cli_port} -a ${redis_cli_password}" C-m
 
     # workspace
     ${tmux} new-window -d -n workspace -t ${project_michael}
-    ${tmux} send-keys -t "${project_michael}:workspace" 'cd /home/html/michael' C-m
+    ${tmux} send-keys -t "${project_michael}:workspace" "cd ${work_path}" C-m
 
     # x
     ${tmux} new-window -d -n x -t ${project_michael}
-    ${tmux} send-keys -t "${project_michael}:x" 'cd /home/html/michael' C-m
-
+    ${tmux} send-keys -t "${project_michael}:x" "cd ${work_path}" C-m
 
     ${tmux} select-window -t vim
 
 }
 
-#${tmux} has-session -t ${project_call}
-#if [ $? != 0 ]; then
-#    init_project_call
-#else
-#    ${tmux} kill-session -t ${project_call}
-#    init_project_call
-#fi
-
-${tmux} has-session -t ${project_michael}
+${tmux} has-session -t ${project_template}
 if [ $? != 0 ]; then
-    init_project_michael
+    init_project_template
 else
-    ${tmux} kill-session -t ${project_michael}
-    init_project_michael
+    ${tmux} kill-session -t ${project_template}
+    init_project_template
 fi
 
-${tmux} -2 attach-session -t ${project_michael}
+#${tmux} has-session -t ${project_michael}
+#if [ $? != 0 ]; then
+#    init_project_michael
+#else
+#    ${tmux} kill-session -t ${project_michael}
+#    init_project_michael
+#fi
+
+${tmux} -2 attach-session -t ${project_template}
+#${tmux} -2 attach-session -t ${project_michael}
 
